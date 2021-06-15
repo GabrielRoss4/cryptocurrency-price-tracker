9e31d76642d4ef732d8e8b8a149c6ba0035bc292
Gabe <Gabriel.Ross@outlook.com>
2021-06-15 18:02:26 -0400

    Included config file

---
+++ config.py
@@ -0,0 +1,24 @@
+from src.query_helper import CoinQuery
+import os
+
+
+DATABASE_PATH = "historic-crypto-prices.db"
+API_KEY = os.environ.get("API_KEY")
+
+if API_KEY is None:
+    print("No API key provided. Exiting...")
+    exit()
+
+COINS_OF_INTEREST = [
+    CoinQuery(coin_name="Bitcoin", coin_symbol="BTC"),
+    CoinQuery(coin_name="Ethereum", coin_symbol="ETH"),
+    CoinQuery(coin_name="Stellar", coin_symbol="XLM"),
+    CoinQuery(coin_name="Litecoin", coin_symbol="LTC"),
+    CoinQuery(coin_name="Ripple", coin_symbol="XRP"),
+    CoinQuery(coin_name="Polkadot", coin_symbol="DOT"),
+    CoinQuery(coin_name="Dogecoin", coin_symbol="DOGE"),
+    CoinQuery(coin_name="BitcoinCash", coin_symbol="BCH"),
+    CoinQuery(coin_name="Cardano", coin_symbol="ADA"),
+    CoinQuery(coin_name="Uniswap", coin_symbol="UNI"),
+    CoinQuery(coin_name="Polygon", coin_symbol="MATIC")
+]
